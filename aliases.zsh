a1a6eaa68fa39936bdefcd000da9bcc34fe1e809
Emil Mazik <emilmazik@MacBook-Pro-Emil.local>
2025-03-24 22:01:51 +0100

    added aliases

---
+++ aliases.zsh
@@ -0,0 +1,13 @@
+# Directories
+alias library="cd $HOME/Library"
+alias repos="cd $HOME/Documents/repos"
+
+# Git
+alias gs="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias gl="git log --oneline --decorate --color"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias pull="git pull"
+alias push="git push"
